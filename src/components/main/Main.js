9b36640572bb2b057c1bd9f593e74d4a2895007e
unknown <Morteza Fathnia>
2019-07-13 14:46:39 +0430

    not found added

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Main extends Component {
   render() {
-    return <div>Main is working!</div>;
+    return <div className={`col-12 text-right mt-2`}>به ژاک خوش آمدید ...</div>;
   }
 }
 export default Main;
--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,12 +1,20 @@
 import React from "react";
-
+import Logo from "../../assets/img/404.png";
+import classes from "./NotFound.module.sass";
 export default () => {
   return (
-    <div>
-      <h1 className="col-4">
-        <span className="text-danger">404</span> Page Not Fonud
-      </h1>
-      <p className="lead">Sorry, that page does not exist</p>
+    <div class="container">
+      <div className={`${classes.main}`}>
+        <div className={`${classes.img_404}`}>
+          <img src={Logo} />
+        </div>
+        <p className={`${classes.text_404}`}>
+          متاسفیم,ما نتونستیم صفحه مورد نظر شما رو پیدا کنیم!
+        </p>
+        <div class={`${classes.btn}`}>
+          <a href="/">منو از اینجا ببر</a>
+        </div>
+      </div>
     </div>
   );
 };
