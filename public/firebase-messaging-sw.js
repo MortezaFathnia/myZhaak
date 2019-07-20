9d7f2a2a24fadb9cf7f03e5d02173d43d043b2f2
MortezaFathnia <fathniamorteza@gmail.com>
2019-07-20 16:27:28 +0430

    firebase service worker added

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,28 +1,14 @@
-importScripts('https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js');
-firebase.initializeApp({
-  // Project Settings => Add Firebase to your web app
-  messagingSenderId: '1062407524656'
-});
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+var firebaseConfig = {
+  apiKey: 'AIzaSyCO0POv68FmCPNhexLnSpGg0wDx40zZqA0',
+  authDomain: 'zhaak-4ad4e.firebaseapp.com',
+  databaseURL: 'https://zhaak-4ad4e.firebaseio.com',
+  projectId: 'zhaak-4ad4e',
+  storageBucket: 'zhaak-4ad4e.appspot.com',
+  messagingSenderId: '242627529388',
+  appId: '1:242627529388:web:ce695ee1d62716ea'
+};
+firebase.initializeApp(firebaseConfig);
+
 const messaging = firebase.messaging();
-messaging.setBackgroundMessageHandler(function(payload) {
-  const promiseChain = clients
-    .matchAll({
-      type: 'window',
-      includeUncontrolled: true
-    })
-    .then(windowClients => {
-      for (let i = 0; i < windowClients.length; i++) {
-        const windowClient = windowClients[i];
-        windowClient.postMessage(payload);
-      }
-    })
-    .then(() => {
-      return registration.showNotification('my notification title');
-    });
-  return promiseChain;
-});
-self.addEventListener('notificationclick', function(event) {
-  // do what you want
-  // ...
-});
